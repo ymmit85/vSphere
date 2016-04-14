eac22744905c26c3e436d9f0504074eb9147d556
Tim Williams <tim@ymmit.net>
2016-04-14 19:54:32 +0800

    Disable SSH
    Disable SSH on all ESXi hosts

---
+++ Disable_SSH.ps1
@@ -0,0 +1,2 @@
+﻿Get-VMHost | Foreach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"} ) -Confirm:$False} | Out-Null
+Get-VMHost | Get-VMHostService | Where { $_.Key -eq "TSM-SSH" } |select VMHost, Label, Running, Policy | FT -AutoSize
