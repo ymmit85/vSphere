bdedacc549fe2f3ab5afa172585763d46384e7d4
Tim Williams <tim@ymmit.net>
2016-04-14 20:10:59 +0800

    vSphere_PSP_SATP_Report.ps1

---
+++ vSphere_PSP_SATP_Report.ps1
@@ -0,0 +1,20 @@
+﻿$lunpathinfo = @()
+foreach ($vmhost in get-vmhost) {
+ 
+#foreach ($vmhost in get-cluster "SAP_DEV_TEST" | get-vmhost) {
+$hostview= get-view $vmhost.id
+$hostview.config.storagedevice.multipathinfo.lun | % { `
+    $lunname=$_.id
+    $lunpolicy=$_.policy.policy
+    $lunsatp=$_.storagearraytypepolicy.policy
+    $_.path | % {
+        $pathstate=$_.pathstate
+        $lunpathinfo += "" | select @{name="Hostname"; expression={$vmhost.name}},
+                                    @{name="LunName"; expression={$lunname}},
+                                    @{name="LunPolicy"; expression={$lunpolicy}},
+                                    @{name="LunSATP"; expression={$lunsatp}},
+                                    @{name="PathState"; expression={$pathstate}}
+    }
+}
+}
+$lunpathinfo | export-csv c:\scripts\host_luns.csv
