fbb7cc63f47719a06a2c07c18434bfa41c535787
Tim Williams <tim@ymmit.net>
2016-04-14 20:06:15 +0800

    Disable SSH
    Disable SSH on all hosts

--- Disable_SSH.ps1
+++ Disable_SSH.ps1
@@ -1,2 +1,3 @@
-﻿Get-VMHost | Foreach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"} ) -Confirm:$False} | Out-Null
+﻿#version 1
+Get-VMHost | Foreach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"} ) -Confirm:$False} | Out-Null
 Get-VMHost | Get-VMHostService | Where { $_.Key -eq "TSM-SSH" } |select VMHost, Label, Running, Policy | FT -AutoSize
