25e5be9d86c1d031205f6df346839be67e779b68
Tim Williams <tim@ymmit.net>
2018-11-06 20:52:46 +0800

    dev

--- upgrade VM Hardware.ps1
+++ upgrade VM Hardware.ps1
@@ -6,10 +6,10 @@ $vmVersion = "v11"
 Do {$vmStatus = get-vm -name $vm;  write-Host "Waiting for $vm to Shutdown... $(get-date -f HH:mm:ss)" -ForegroundColor Yellow; sleep -s 10}
             until ($vmStatus.PowerState -eq "PoweredOff")
 
-write-host "yay! $vm is off now.. upgrading hardware"
+write-host "$vm is off now.. upgrading hardware"
 
 set-vm -vm (get-vm $vm) -Version $vmVersion -Confirm:$false
 $x = get-vm $vm
-write-host "Hardware on $vm upgraded to "$x.version"... starting it back up"
+write-host "Hardware on $vm upgraded to "$x.version"... Starting it back up"
 
 start-vm -vm $vm -confirm:$false | Out-Null
